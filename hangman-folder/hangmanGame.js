3460516ad70d72deeb264a27bb47373ed4fffa81
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-04-19 11:08:14 -0500

    Built logic to have displayedArray show up within the game
    - Need to get the guessCounter to only decrement when the currentGuess is not in the wordToGuess

--- hangman-folder/hangmanGame.js
+++ hangman-folder/hangmanGame.js
@@ -4,38 +4,50 @@ let wordToGuessArray = wordToGuess.split("");
 let displayedArray = [];
 let incorrectLettersArray = [];
 let guessCounter = 6;
+let currentGuess;
 
 //[A,E,T] === [E,A,T]
 
+initializeDisplayedArray();
+updateDisplayedArray();
 //While loop with conditions to end game - inside While loop will be our gameplay
-while (guessCounter !== 0 && displayedArray !== wordToGuessArray) {
+while (guessCounter > 0 && displayedArray !== wordToGuessArray) {
   //prompt user for a letter to guess
   console.log('Guesses remaining:', guessCounter);
-  let currentGuess = prompt('What letter do you guess? :')
-
-  //adds a counter to our guess
-  guessCounter--;
+  console.log('Guess this word', displayedArray);
+   currentGuess = prompt('What letter do you guess? :');
+   for (let i = 0; i < wordToGuessArray.length; i++) {
+    if (currentGuess === wordToGuessArray[i]) {
+      updateDisplayedArray();
+     }
+     else {
+      guessCounter--;
+     }
+   }
 }
 
+guessCounter = 6;
 
 //logs and tests
 console.log('This is the word you should have guessed -->', wordToGuess);
 
+
 function updateDisplayedArray() {
   
   for (let i = 0; i < wordToGuessArray.length; i++) {
+    if (displayedArray[i] === '_'){
+      if (currentGuess ===  wordToGuessArray[i]) {
+      // add the guess letter to the array
+      displayedArray[i]=currentGuess;
+      }
+      else{
+        displayedArray[i]='_';
+      }
+  }}
+}
 
-  if (currentGuess ===  wordToGuessArray[i]) {
-    // add the guess letter to the array
-    displayedArray[i]=currentGuess;
-  }
-  else{
+function initializeDisplayedArray() {
+  for (let i = 0; i < wordToGuessArray.length; i++) {
     displayedArray[i]='_';
   }
-  
 }
-}
-
-
-
-console.log(displayedArray);
