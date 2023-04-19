04cdc44b10e2d3f03d2efd01325a97a21e01e940
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-04-19 13:38:44 -0500

    Logic for game is now built out

--- hangman-folder/hangmanGame.js
+++ hangman-folder/hangmanGame.js
@@ -1,4 +1,4 @@
-const prompt = require('prompt-sync')();
+const prompt = require('prompt-sync')({sigint: true});
 let wordToGuess = 'citrine';
 let wordToGuessArray = wordToGuess.split("");
 let displayedArray = [];
@@ -7,43 +7,42 @@ let guessCounter = 6;
 let currentGuess;
 
 //[A,E,T] === [E,A,T]
-
+//[E,A,T] !== [E,A,T] when objects
 initializeDisplayedArray();
 updateDisplayedArray();
 //While loop with conditions to end game - inside While loop will be our gameplay
-while (guessCounter > 0 && displayedArray !== wordToGuessArray) {
+while (guessCounter !== 0 && displayedArray.toString() !== wordToGuessArray.toString()) {
   //prompt user for a letter to guess
-  console.log('Guesses remaining:', guessCounter);
+
   console.log('Guess this word', displayedArray);
-   currentGuess = prompt('What letter do you guess? :');
-   for (let i = 0; i < wordToGuessArray.length; i++) {
-    if (currentGuess === wordToGuessArray[i]) {
-      updateDisplayedArray();
-     }
-     else {
-      guessCounter--;
-     }
-   }
+
+  currentGuess = prompt('What letter do you guess? :');
+
+  updateDisplayedArray();
+
+  if (wordToGuessArray.includes(currentGuess)) {
+    console.log('CORRECT...dang you smart');
+  } else {
+    console.log('WRONG TRY AGAIN');
+    incorrectLettersArray.push(currentGuess);
+    guessCounter--;
+  }
+  console.log('Guesses remaining:', guessCounter);
 }
 
-guessCounter = 6;
+
 
 //logs and tests
 console.log('This is the word you should have guessed -->', wordToGuess);
 
 
 function updateDisplayedArray() {
-  
   for (let i = 0; i < wordToGuessArray.length; i++) {
-    if (displayedArray[i] === '_'){
-      if (currentGuess ===  wordToGuessArray[i]) {
+    if (currentGuess ===  wordToGuessArray[i]) {
       // add the guess letter to the array
-      displayedArray[i]=currentGuess;
-      }
-      else{
-        displayedArray[i]='_';
-      }
-  }}
+      displayedArray[i] = currentGuess;
+    }
+  }
 }
 
 function initializeDisplayedArray() {
