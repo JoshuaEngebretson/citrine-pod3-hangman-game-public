dbc70c9ae085d48ec58981d828b0c5174eb1cdbb
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-04-19 14:45:11 -0500

    Additional features added to game
    - Word to guess randomizes each time the game is reset
    - Prompt to ask the user if they want to play Hangman before starting the game.
    	- If they do not, this ends the application.
    - Adjusted formatting of spaces between game play rounds.
    - Added emojis to console logs

--- hangman-folder/hangmanGame.js
+++ hangman-folder/hangmanGame.js
@@ -1,39 +1,86 @@
 const prompt = require('prompt-sync')({sigint: true});
+
 let wordToGuess = 'citrine';
 let wordToGuessArray = wordToGuess.split("");
 let displayedArray = [];
 let incorrectLettersArray = [];
 let guessCounter = 6;
 let currentGuess;
-
+let wordsForGame = [
+  'quokka', 'potato', 'citrine',
+  'unicorn', 'jquery', 'tangerine',
+  'prime', 'harasymiw', 'express',
+  'database'
+];
 //[A,E,T] === [E,A,T]
 //[E,A,T] !== [E,A,T] when objects
-initializeDisplayedArray();
-updateDisplayedArray();
-//While loop with conditions to end game - inside While loop will be our gameplay
-while (guessCounter !== 0 && displayedArray.toString() !== wordToGuessArray.toString()) {
-  //prompt user for a letter to guess
+// initializeDisplayedArray();
+// updateDisplayedArray();
+// playHangman();
 
-  console.log('Guess this word', displayedArray);
+gameReset();
 
-  currentGuess = prompt('What letter do you guess? :');
+function playHangman() {
+  //While loop with conditions to end game - inside While loop will be our gameplay
+  while (guessCounter !== 0 && displayedArray.toString() !== wordToGuessArray.toString()) {
+    // put some space in between each guess:
+    clearSmallerScreen();
+    console.log('Guess this word', displayedArray);
 
-  updateDisplayedArray();
 
-  if (wordToGuessArray.includes(currentGuess)) {
-    console.log('CORRECT...dang you smart');
-  } else {
-    console.log('WRONG TRY AGAIN');
-    incorrectLettersArray.push(currentGuess);
-    guessCounter--;
+    console.log('Here are the incorrect letters you have guessed so far \n', incorrectLettersArray);
+    //prompt user for a letter to guess & show them the updated display:
+    currentGuess = prompt('What letter do you guess? --> ');
+    updateDisplayedArray();
+
+    if (wordToGuessArray.includes(currentGuess)) {
+      console.log('CORRECT...dang you smart');
+    } else {
+      console.log('WRONG TRY AGAIN');
+      incorrectLettersArray.push(currentGuess);
+      guessCounter--;
+    }
+    console.log('Guesses remaining:', guessCounter);
+
   }
-  console.log('Guesses remaining:', guessCounter);
+
+  if (displayedArray.toString() === wordToGuessArray.toString()) {
+    //Win or Lose console logs
+    console.log('Big Winner YAY! The word was -->', wordToGuess);
+    gameReset();
+    // playHangman();
+  }
+  else {
+    //Lose message
+    console.log('This was the secret word -->', wordToGuess);
+    console.log('Better luck next time 🥲');
+    gameReset();
+    // playHangman();
+  }
 }
 
 
 
-//logs and tests
-console.log('This is the word you should have guessed -->', wordToGuess);
+function gameReset() {
+  // wordToGuess = 'citrine';
+  
+  //randomize the new word
+  wordToGuess = wordsForGame[randomNum()];
+  console.log('word to guess -->', wordToGuess);
+  // Adjust the word into an array for means of looping
+  wordToGuessArray = wordToGuess.split("");
+  //Clear out the displayedArray
+  displayedArray = [];
+  //
+  incorrectLettersArray = [];
+  guessCounter = 6;
+  currentGuess = "";
+
+  initializeDisplayedArray();
+  updateDisplayedArray();
+  wantToPlay();
+
+}
 
 
 function updateDisplayedArray() {
@@ -48,5 +95,45 @@ function updateDisplayedArray() {
 function initializeDisplayedArray() {
   for (let i = 0; i < wordToGuessArray.length; i++) {
     displayedArray[i]='_';
+  }
+}
+
+function clearSmallerScreen() {
+  console.log('\n\n');
+}
+
+function clearScreen() {
+  console.log('\n\n\n\n');
+  console.log('\t\t Game Reset \n \t 👾👾👾👾👾👾👾👾👾👾👾');
+}
+
+function randomNum(){
+  let min = 0;
+  let max = wordsForGame.length;
+  
+  return Math.floor(Math.random() * max); 
+}
+
+
+
+
+
+function wantToPlay() {
+  
+  clearSmallerScreen();
+  
+  let userWantToPlay = prompt('Do you want to play Hangman? - Enter "yes" or "no": ');
+  userWantToPlay = userWantToPlay.toLowerCase()
+
+  if (userWantToPlay === 'yes' || userWantToPlay === 'y') {
+    console.log(`Let's play Hangman!`);
+    clearScreen();
+    playHangman();
+  }
+  else if (userWantToPlay === 'no'|| userWantToPlay === 'n') {
+    console.log('\nCool, thanks for running the application.');
+    clearSmallerScreen();
+  } else {
+    console.log('Hey... please choose "yes" or "no" \n 😡');
   }
 }
